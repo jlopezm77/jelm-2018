e5b72c268feb6222c6a81d02b8ee26cd53dc13dd
Julio Enrique <juelopezma@gmail.com>
2018-08-06 20:59:00 -0500

    devise y API

--- enrique/app/controllers/api_controller.rb
+++ enrique/app/controllers/api_controller.rb
@@ -5,4 +5,14 @@ class ApiController < ApplicationController
         books: books
         }
     end
+
+    def show
+            book_id = params[:book_id]
+            book = Book.find(book_id)
+            render json: 
+            {
+                id: book.id,
+                name: book.name
+            }
+    end
 end
--- enrique/config/routes.rb
+++ enrique/config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   root to: "boostore#index"
   get "autores", to: 'author#index'
   get "api/libros", to: "api#books"
+  get "api/libro/:book_id", to: "api#show"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   
